59d7d087c28888b0ee41d6f42e7e5d0aea0eb192
Teri Forey <teri.forey@gmail.com>
2021-08-09 09:23:51 +0100

    Initial commit

---
+++ core/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    pass
---
+++ core/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def main(request):
+    return render(request, "core/main.html")
